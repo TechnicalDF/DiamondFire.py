1d3833fab356dfc5dbe3123c359b9f7ddf98f568
HellishBro <hellishbroamongus@gmail.com>
2025-01-26 17:07:03 -0800

    changed how cc works & tokens

--- src/df/codeclient.py
+++ src/df/codeclient.py
@@ -6,6 +6,7 @@ from enum import Flag, auto
 from amulet_nbt import CompoundTag, from_snbt, ListTag
 
 class CCOutOfScopeError(Exception): pass
+class CCInvalidToken(Exception): pass
 
 class CCAuthScopes(Flag):
     DEFAULT = auto()
@@ -40,6 +41,16 @@ class CodeClient:
         self._scopes = CCAuthScopes.DEFAULT
         self.__method._api = self
 
+        self.scopes = self.__scopes()
+        self.give = self.__give()
+        self.token = self.__token()
+        self.inv = self.__inv()
+        self.spawn = self.__spawn()
+        self.mode = self.__mode()
+        self.plot = self.__plot()
+        self.place = self.__place()
+        self.clear_plot = self.__clear_plot()
+
     def have_scope(self, scope: CCAuthScopes) -> bool:
         """
         Checks if the current session have the scope.
@@ -61,7 +72,7 @@ class CodeClient:
     def _ts(data: str | bytes) -> str:
         return data if isinstance(data, str) else data.decode()
 
-    def recv(self) -> str:
+    def _recv(self) -> str:
         return self._ts(self.socket.recv(self.timeout))
 
     class __method:
@@ -74,13 +85,13 @@ class CodeClient:
             if not self.scope in self._api._scopes:
                 raise CCOutOfScopeError(f"The action require scope {self.scope.name}.")
 
-    class give(__method):
+    class __give(__method):
         """
         Requires DEFAULT scope.
         """
         scope = CCAuthScopes.DEFAULT
 
-        def __init__(self, item: str | CompoundTag):
+        def __call__(self, item: str | CompoundTag):
             """
             Sends an item (SNBT) to Minecraft.
             :param item: The ItemValue in SNBT or a Compound NBT Tag.
@@ -90,24 +101,53 @@ class CodeClient:
                 item = item.to_snbt()
             self._api.socket.send("give " + item)
             try:
-                message = self._api.recv()
+                message = self._api._recv()
                 if message == "not creative mode":
                     raise SendToMinecraftError("The player is not in creative mode.")
             except TimeoutError:
                 pass
 
-    class scopes(__method):
+    class __token(__method):
         """
         Requires DEFAULT scope.
         """
         scope = CCAuthScopes.DEFAULT
 
-        def __init__(self):
+        def get(self) -> str:
+            """
+            Gets a token that is authenticated with the currently approved scopes.
+            :return: The token that can be used to authenticate with.
+            """
+            self._api.socket.send("token")
+            token = self._api._recv()
+            return token
+
+        def authenticate(self, token: str):
+            """
+            Authenticates using the token.
+            :param token: The token attached to the scopes.
+            :raises CCInvalidToken: If the token is invalid.
+            """
+            self._api.socket.send("token " + token)
+            message = self._api._recv()
+            if message == "invalid token":
+                raise CCInvalidToken()
+            elif message == "auth":
+                self._api.scopes()
+
+    class __scopes(__method):
+        """
+        Requires DEFAULT scope.
+        """
+        scope = CCAuthScopes.DEFAULT
+
+        def __call__(self) -> CCAuthScopes:
             """
             Gets the list of scopes the program is authorized with.
+            :return: The list of scopes
             """
             self._api.socket.send("scopes")
-            message = self._api.recv()
+            message = self._api._recv()
             scopes = message.split()
             new_scopes = CCAuthScopes.DEFAULT
             if "inventory" in scopes:
@@ -121,10 +161,12 @@ class CodeClient:
             if "clear_plot" in scopes:
                 new_scopes |= CCAuthScopes.CLEAR_PLOT
 
-        @classmethod
-        def request(cls, requested_scopes: CCAuthScopes, timeout: int = None):
+            self._api._scopes = new_scopes
+            return new_scopes
+
+        def request(self, requested_scopes: CCAuthScopes, timeout: int = None):
             """
-            Requests new scopes for the program.
+            Requests new scopes for the program and continues when the player runs /auth.
             :param requested_scopes: The new scopes.
             :param timeout: Timeout for player input in seconds.
             :raises TimeoutError: If the request times out.
@@ -140,26 +182,25 @@ class CodeClient:
                 scope_list.append("write_code")
             if CCAuthScopes.CLEAR_PLOT in requested_scopes:
                 scope_list.append("clear_plot")
-            cls._api.socket.send("scopes " + " ".join(scope_list))
-            if "auth" in cls._api._ts(cls._api.socket.recv(timeout)):
-                cls._api._scopes = requested_scopes
+            self._api.socket.send("scopes " + " ".join(scope_list))
+            if "auth" in self._api._ts(self._api.socket.recv(timeout)):
+                self._api._scopes = requested_scopes
 
-    class inv(__method):
+    class __inv(__method):
         """
         Requires INVENTORY scope.
         """
         scope = CCAuthScopes.INVENTORY
 
-        @classmethod
-        def get(cls) -> list[CompoundTag]:
+        def get(self) -> list[CompoundTag]:
             """
             Gets the player's inventory.
             :return: List of items in CompoundTag.
             :raises socket.timeout: If the request times out.
             """
-            cls()._require_scope()
-            cls._api.socket.send("inv")
-            items = cls._api.recv()
+            self._require_scope()
+            self._api.socket.send("inv")
+            items = self._api._recv()
             parsed: ListTag = from_snbt(items)
             decoded: list[CompoundTag] = []
             for item in parsed:
@@ -167,14 +208,13 @@ class CodeClient:
             
             return decoded
         
-        @classmethod
-        def set(cls, items: list[CompoundTag | str]):
+        def set(self, items: list[CompoundTag | str]):
             """
             Sets the player's inventory.
             :param items: List of item in SNBT or a Compound NBT Tag.
             :raises SendToMinecraftError: If the player is not in creative mode.
             """
-            cls()._require_scope()
+            self._require_scope()
 
             parsed = []
             for item in parsed:
@@ -182,98 +222,100 @@ class CodeClient:
                     item = item.to_snbt()
                 parsed.append(item)
 
-            cls._api.socket.send("setinv [" + ",".join(parsed) + "]")
+            self._api.socket.send("setinv [" + ",".join(parsed) + "]")
             try:
-                message = cls._api.recv()
+                message = self._api._recv()
                 if message == "not creative mode":
                     raise SendToMinecraftError("The player is not in creative mode.")
             except TimeoutError:
                 pass
 
-    class spawn(__method):
+    class __spawn(__method):
         """
         Requires MOVEMENT scope.
         """
         scope = CCAuthScopes.MOVEMENT
 
-        def __init__(self):
+        def __call__(self):
             """
             Moves the player to the codespace spawn.
             """
             self._require_scope()
             self._api.socket.send("spawn")
 
-    class mode(__method):
+    class __mode(__method):
         """
         Requires MOVEMENT scope.
         """
         scope = CCAuthScopes.MOVEMENT
 
-        @classmethod
-        def get(cls) -> Mode:
+        def get(self) -> Mode:
             """
             Gets the player's current mode.
             :return: The player's mode.
             :raises socket.timeout: If the request times out.
             """
-            cls()._require_scope()
-            cls()._api.socket.send("mode")
-            mode = cls._api.recv()
+            self._require_scope()
+            self._api.socket.send("mode")
+            mode = self._api._recv()
             match mode:
                 case "spawn": return Mode.SPAWN
                 case "play": return Mode.PLAY
                 case "dev": return Mode.DEV
                 case "build": return Mode.BUILD
 
-        @classmethod
-        def set(cls, mode: Mode):
+
+        def set(self, mode: Mode):
             """
             Sets the player's current mode. To confirm, use ``mode.get()`` again and compare.
             :param mode: The new mode to set to.
             """
-            cls()._require_scope()
-            cls()._api.socket.send("mode " + mode.value)
+            self._require_scope()
+            self._api.socket.send("mode " + mode.value)
 
-    class plot(__method):
+    class __plot(__method):
         """
         Requires READ_PLOT scope.
         """
         scope = CCAuthScopes.READ_PLOT
 
-        @classmethod
-        def get_templates(cls) -> list[Template]:
+
+        def get_templates(self) -> list[Template]:
             """
             Gets all templates of the plot. It's recommended to set a high timeout value for this.
             :return: The list of templates.
             :raises socket.timeout: If the request times out.
             """
-            cls()._require_scope()
-            cls()._api.socket.send("scan")
-            data = cls._api.recv()
+            self._require_scope()
+            self._api.socket.send("scan")
+            data = self._api._recv()
             templates = []
             for template in data.split("\n"):
                 templates.append(Template.decompress(template))
             return templates
 
-        @classmethod
-        def get_size(cls) -> PlotSize:
+
+        def get_size(self) -> PlotSize:
             """
             Gets the current plot size.
             :return: The plot size.
             :raises socket.timeout: If the request times out.
             """
-            cls()._require_scope()
-            cls()._api.socket.send("size")
-            size = cls._api.recv()
+            self._require_scope()
+            self._api.socket.send("size")
+            size = self._api._recv()
             return PlotSize(size)
 
-    class place(__method):
+    class __place(__method):
         """
         Requires WRITE_CODE scope.
         """
         scope = CCAuthScopes.WRITE_CODE
 
-        def __init__(self, compact: bool = False):
+        def __init__(self):
+            self.compact = False
+
+        def __call__(self, compact: bool = False):
             """
             A builder. To actually place the code, call ``place.execute()``
             """
@@ -318,13 +360,13 @@ class CodeClient:
             self._require_scope()
             self._api.socket.send("place go")
 
-    class clear_plot(__method):
+    class __clear_plot(__method):
         """
         Requires CLEAR_PLOT scope.
         """
         scope = CCAuthScopes.CLEAR_PLOT
 
-        def __init__(self):
+        def __call__(self):
             """
             Clears the codespace. **USE WITH CAUTION** as this will remove all code.
             """
